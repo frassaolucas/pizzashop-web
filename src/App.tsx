ac52f70064ac7278324e95783e5b1d41d3704dac
Lucas Frassão <frassao.lucas@gmail.com>
2024-03-08 16:14:28 -0300

    feature: addded dark theme + finished header

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Toaster richColors />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Toaster richColors />
 
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <RouterProvider router={router} />
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
