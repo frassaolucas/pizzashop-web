b8856deb85b90d459f4e18b1686bd7da9bf1b5d8
Lucas Frassão <frassao.lucas@gmail.com>
2024-04-01 08:29:08 -0300

    feat: created 404 page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { NotFound } from './pages/404'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard/dashboard'
@@ -11,29 +12,19 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
-      {
-        path: '/',
-        element: <Dashboard />,
-      },
-      {
-        path: '/orders',
-        element: <Orders />,
-      },
+      { path: '/', element: <Dashboard /> },
+      { path: '/orders', element: <Orders /> },
     ],
   },
+
   {
     path: '/',
     element: <AuthLayout />,
     children: [
-      {
-        path: '/sign-in',
-        element: <SignIn />,
-      },
-      {
-        path: '/sign-up',
-        element: <SignUp />,
-      },
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> },
     ],
   },
 ])
